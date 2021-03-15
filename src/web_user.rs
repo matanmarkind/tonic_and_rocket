5bdd54db8938993483b6e5e1c753c6184d7a641b
Matan Markind <matan.markind@gmail.com>
2021-03-15 20:58:53 +0200

    Improve on rocket usage. sage. Gives multiple examples of reading from files and streaming responses from a backend server.

--- src/web_user.rs
+++ src/web_user.rs
@@ -1,5 +1,7 @@
 /// Uses the route_guide RouteGuideClient service, but via RESTfull interface
 /// via Rocket.
+use core::pin::Pin;
+use core::task::{Context, Poll};
 use rocket::response::content::Html;
 use rocket::response::{Debug, Stream};
 use rocket::{get, State};
@@ -7,19 +9,53 @@ use rocket_contrib::serve::{crate_relative, StaticFiles};
 use std::sync::Mutex;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncRead, ReadBuf};
 use tokio::net::{TcpListener, TcpStream};
 use tonic::transport::Channel;
 use tonic::Request;
 
 use routeguide::route_guide_client::RouteGuideClient;
-use routeguide::{Point, Rectangle};
+use routeguide::{Feature, Point, Rectangle};
 
 mod util;
 use util::*;
 
+struct FeatureStream {
+    stream: tonic::Streaming<Feature>,
+}
+
+impl AsyncRead for FeatureStream {
+    // https://stackoverflow.com/questions/66482722/impl-asyncread-for-tonicstreaming
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        use futures::stream::StreamExt;
+        use std::io::{Error, ErrorKind};
+
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        match self.stream.poll_next_unpin(cx) {
+            Poll::Ready(Some(Ok(m))) => {
+                buf.put_slice(format!("{:?}\n", m).as_bytes());
+                Poll::Ready(Ok(()))
+            }
+            Poll::Ready(Some(Err(e))) => {
+                Poll::Ready(Err(Error::new(ErrorKind::Other, format!("{:?}", e))))
+            }
+            Poll::Ready(None) => {
+                // None from a stream means the stream terminated. To indicate
+                // that from AsyncRead we return Ok and leave buf unchanged.
+                Poll::Ready(Ok(()))
+            }
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
 #[get("/")]
 fn index() -> Html<String> {
-    Html(std::fs::read_to_string(crate_relative!("data/static_webpages/search.html")).unwrap())
+    Html(std::fs::read_to_string(crate_relative!("data/static_webpages/index.html")).unwrap())
 }
 
 #[get("/searchstream")]
@@ -50,24 +86,54 @@ async fn feature(
     format!("{:?}", response.into_inner())
 }
 
-#[get("/list_features")]
+#[get("/list_features2")]
+async fn list_features2(
+    client: State<'_, RouteGuideClient<Channel>>,
+) -> Result<Stream<FeatureStream>, Debug<std::io::Error>> {
+    let mut client = client.inner().clone();
+    let stream = client
+        .list_features(Request::new(Rectangle {
+            low: Some(Point {
+                latitude: 400000000,
+                longitude: -750000000,
+            }),
+            high: Some(Point {
+                latitude: 420000000,
+                longitude: -730000000,
+            }),
+        }))
+        .await
+        .unwrap()
+        .into_inner();
+
+    Ok(Stream::from(FeatureStream { stream }))
+}
+
+#[get("/list_features?<low_lat>&<low_long>&<high_lat>&<high_long>")]
 async fn list_features(
     client: State<'_, RouteGuideClient<Channel>>,
     tasks: State<'_, Mutex<Vec<tokio::task::JoinHandle<()>>>>,
+    low_lat: i32,
+    low_long: i32,
+    high_lat: i32,
+    high_long: i32,
 ) -> Result<Stream<TcpStream>, Debug<std::io::Error>> {
-    let rectangle = Rectangle {
-        low: Some(Point {
-            latitude: 400_000_000,
-            longitude: -750_000_000,
-        }),
-        high: Some(Point {
-            latitude: 420_000_000,
-            longitude: -730_000_000,
-        }),
-    };
+    // low_lat: 400000000,
+    // low_long: -750000000,
+    // high_lat: 420000000,
+    // high_long: -730000000,
     let mut client = client.inner().clone();
     let mut feature_stream = client
-        .list_features(Request::new(rectangle))
+        .list_features(Request::new(Rectangle {
+            low: Some(Point {
+                latitude: low_lat,
+                longitude: low_long,
+            }),
+            high: Some(Point {
+                latitude: high_lat,
+                longitude: high_long,
+            }),
+        }))
         .await
         .unwrap()
         .into_inner();
@@ -115,7 +181,7 @@ async fn rocket() -> rocket::Rocket {
         .manage(Mutex::new(Vec::<tokio::task::JoinHandle<()>>::new()))
         .mount(
             "/",
-            rocket::routes![index, list_features, searchstream, feature],
+            rocket::routes![index, list_features, list_features2, searchstream, feature],
         )
         .mount(
             "/static",
